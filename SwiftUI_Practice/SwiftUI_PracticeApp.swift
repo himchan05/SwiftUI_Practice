3d603d6b197fde751c523786a28262f8e8cc8e45
박힘찬 <himchan05@naver.com>
2022-03-02 12:48:53 +0900

    Initial Commit

---
+++ SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by 박힘찬 on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
